75bc41226c21eae79d657835ef1b11c9d39abcef
kavery-pm <shiny.kavery@gmail.com>
2023-01-24 05:41:44 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        shiny protfolio
         </a>
       </header>
     </div>

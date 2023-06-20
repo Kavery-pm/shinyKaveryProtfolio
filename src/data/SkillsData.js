fca9c9e9936eff408d01786649156db5dcf0d94a
kavery-pm <shiny.kavery@gmail.com>
2023-06-20 17:19:31 +0200

    added the skillsData

---
+++ src/data/SkillsData.js
@@ -0,0 +1,15 @@
+export const skillsData = [
+    'HTML',
+    'Javascript',
+    'CSS',
+    'React',
+    ' Angular',
+    'Node JS',
+    'Bootstrap',
+    'ViteJS',
+    'MaterialUI',
+    'MongoDB',
+    'MySQL',
+    'Figma'
+   
+]

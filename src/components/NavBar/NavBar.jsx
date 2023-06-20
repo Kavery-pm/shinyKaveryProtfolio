84bb39949a281c29d88c33d9ec3487560cff8d9b
kavery-pm <shiny.kavery@gmail.com>
2023-06-20 09:29:34 +0200

    added the react-reveal package and Navbar styling

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import './NavBar.css';
+import Fade from 'react-reveal/Fade';
+const NavBar = ()=>{
+return (
+    <>
+    <Fade>
+        hi
+    </Fade>
+    </>
+)
+}
+export default NavBar;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,13 @@
 import { Helmet } from "react-helmet"
 import { headerData } from '../data/headerData'
+import NavBar from "../components/NavBar/NavBar";
 const Main = ()=>{
     return (
         <>
         <Helmet>
         <title>{headerData.name} - Porfolio</title>
         </Helmet>
+        <NavBar/>
         </>
     )
 }

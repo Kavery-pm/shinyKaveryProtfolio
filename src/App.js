9a8c3ac917c5114283986802b3db16c8bc69137a
kavery-pm <shiny.kavery@gmail.com>
2023-06-20 15:23:53 +0200

    added the scroll to top component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Main from './pages/Main';
+import ScrollToTop from './utils/ScrollToTop';
 
 function App() {
   return (
     <Router>
-   
+   <ScrollToTop/>
     <Switch>
       <Route path="/" exact component={Main} />
       

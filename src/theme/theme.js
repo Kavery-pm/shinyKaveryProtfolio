b8bd85a9e433993189335c6f44c8061c5c6fae6a
kavery-pm <shiny.kavery@gmail.com>
2023-06-20 10:12:10 +0200

    added a theme and the navbar styling

---
+++ src/theme/theme.js
@@ -0,0 +1,19 @@
+
+export const pinkThemeDark = {
+    type: 'dark',
+    primary: '#ff4f93',
+    primary400: '#e56f9d',
+    primary600: '#e14381',
+    primary80: '#ff4f93cc',
+    primary50: '#ff4f9380',
+    primary30: '#ff4f934d',
+    secondary: '#212121',
+    secondary70: '#212121b3',
+    secondary50: '#21212180',
+    tertiary: '#eaeaea',
+    tertiary80: '#eaeaeacc',
+    tertiary70: '#eaeaeab3',
+    tertiary50: '#eaeaea80',
+   
+   
+}

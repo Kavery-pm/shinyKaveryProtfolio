10f23fed51a68668c5598680392a9c85d404e810
kavery-pm <shiny.kavery@gmail.com>
2023-06-21 09:13:30 +0200

    added the projects component

---
+++ src/data/ProjectsData.js
@@ -0,0 +1,12 @@
+import FilterCountry from '../assets/projectImage/FilterCountry.png';
+
+export const ProjectsData = [
+    {
+        id: 1,
+        projectName: 'Country Filter',
+        projectDesc: 'performs a fuzzy search',
+        tags: ['React', 'HTML' ,'CSS', 'Javascript'],
+        code: 'https://github.com/Kavery-pm/CountryFilter',
+        demo: 'https://country-filter.vercel.app/',
+        image: FilterCountry
+    }]

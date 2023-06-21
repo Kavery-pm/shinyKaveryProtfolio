3f2f4abb2dc68c2c22334af5364b9ecb4d0f0bf7
kavery-pm <shiny.kavery@gmail.com>
2023-06-21 10:56:15 +0200

    added the redirect to contact button

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Main from './pages/Main';
 import ScrollToTop from './utils/ScrollToTop';
+import Contacts from './components/Contacts/Contacts';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
    <ScrollToTop/>
     <Switch>
       <Route path="/" exact component={Main} />
-      
+      <Route path="/contacts" exact component={Contacts} />
 
       <Redirect to="/" />
     </Switch>

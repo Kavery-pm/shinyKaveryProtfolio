fdb1ec71d88f4f4b5a2b080f26aeb32ed8f0fa20
kavery-pm <shiny.kavery@gmail.com>
2023-06-22 11:05:25 +0200

    added a new project

--- src/data/AboutMe.js
+++ src/data/AboutMe.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
     description1: "Hello! I am a  Front End Developer based in Erlangen, Germany 🇩🇪, with extensive experience in both full stack and front end development",
-    description2: "Experienced Senior Front End Developer with a strong track record in software development and integration. Skilled in leading development projects, integrating systems and APIs, and delivering high-quality solutions. Proficient in AngularJS, Node.js, MongoDB, SQL, and Agile methodologies. Adept at mentoring junior engineers, resolving defects/bugs, and collaborating with cross-functional teams. Committed to clean code, efficient task completion, and staying up-to-date with emerging technologies.",
+    description2: "Experienced Senior Front End Developer with a strong track record in software development and integration. Skilled in leading development projects, integrating systems and APIs, and delivering high-quality solutions. Proficient in React, AngularJS, Node.js, MongoDB, SQL, and Agile methodologies. Adept at mentoring junior engineers, resolving defects/bugs, and collaborating with cross-functional teams. Committed to clean code, efficient task completion, and staying up-to-date with emerging technologies.",
     image: 2
 }
